fff524ca16d90c52f71c1d5b621889bf58166b41
max11 <vonrut@gmail.com>
2016-01-27 20:54:17 +0300

    makeUsing

--- WatchSite/EasyAnalyze.cs
+++ WatchSite/EasyAnalyze.cs
@@ -46,94 +46,98 @@ namespace WatchSite
         {
             string page = "";
 
-            System.Net.WebClient web = new System.Net.WebClient();
-            web.Encoding = UTF8Encoding.Default;
-            string str1 = web.DownloadString(url);
-
-            str1 = System.Web.HttpUtility.HtmlDecode(str1);
-            var numArr = Regex.Matches(str1, @"itemsCount"":([0-9]{1,5}),")
-            .Cast<Match>()
-            .Select(m => m.Groups[1].Value)
-            .Distinct()
-            .ToArray();
-
-            int num = Int32.Parse(numArr[0]);
-
-            while (num >= 0)
+            using (System.Net.WebClient web = new System.Net.WebClient())
             {
-                num = num - 10;
 
-                string zagotovka = "http://vk.com/al_market.php?al=1&id=-$2&load=1&offset=" + num +
-                "&price_from=&price_to=&q=&sort=0";
-                _url = Regex.Replace(url, @"(http://vk.com/market-)(.+)", zagotovka);
+                web.Encoding = UTF8Encoding.Default;
+                string str1 = web.DownloadString(url);
 
-                string str = web.DownloadString(_url);
-                str = System.Web.HttpUtility.HtmlDecode(str);
+                str1 = System.Web.HttpUtility.HtmlDecode(str1);
+                var numArr = Regex.Matches(str1, @"itemsCount"":([0-9]{1,5}),")
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .Distinct()
+                .ToArray();
 
-                page = page + str; //doc.DocumentNode.InnerHtml
-                Thread.Sleep(Zaderzhka);
+                int num = Int32.Parse(numArr[0]);
+
+                while (num >= 0)
+                {
+                    num = num - 10;
+
+                    string zagotovka = "http://vk.com/al_market.php?al=1&id=-$2&load=1&offset=" + num +
+                    "&price_from=&price_to=&q=&sort=0";
+                    _url = Regex.Replace(url, @"(http://vk.com/market-)(.+)", zagotovka);
+
+                    string str = web.DownloadString(_url);
+                    str = System.Web.HttpUtility.HtmlDecode(str);
+
+                    page = page + str; //doc.DocumentNode.InnerHtml
+                    Thread.Sleep(Zaderzhka);
+                }
+                //<div id="market_item24046" class="market_row">
+                //File.WriteAllText("C:\\Users\\Maxs\\Desktop\\www1.txt", page);
+                //div[@class='market_row_inner_cont']
+                List<Product> listProducts = new List<Product>();
+
+                var tempUrlToItems = Regex.Matches(page, @"name""><a href=""(/market-[0-9]{2,20}\?w=product-[0-9_]{2,35})")
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .ToArray();
+
+                var tempIdToItems = Regex.Matches(page, @"<div id=""market_item([0-9]{1,9})"" class=""market_row"">")
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .ToArray();
+
+                var tempNameToItems = Regex.Matches(page, @"<div class=""market_row_name""><a.*?>(.*?)</a")
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .ToArray();
+
+                var tempPriceToItems = Regex.Matches(page, @"<div class=""market_row_price"">(.*?)</div>")
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .ToArray();
+
+                Product product;
+                for (int i = 0; i < tempUrlToItems.Length; i++)
+                {
+                    product = new Product();
+                    product.Url = Regex.Replace(tempUrlToItems[i], @"/(market-[0-9]{2,20})\?w=(product-[0-9_]{2,35})",
+                        "http://vk.com/wkview.php?act=show&al=1&loc=$1&w=$2");
+                    product.Name = tempNameToItems[i];
+                    product.Id = tempIdToItems[i];
+                    //<div class="market_row_price">2<span class="num_delim"> </span>230 руб.</div>
+                    product.Price = Regex.Replace(tempPriceToItems[i], @"(<div class=""market_row_price"".*?)([7-9]{1,7} руб.)(</div>)",
+                        "$2");
+
+                    listProducts.Add(product);
+                }
+
+                listProducts = listProducts.Distinct(new ProductComparer()).ToList();
+
+
+                str1 = System.Web.HttpUtility.HtmlDecode(str1);
+
+                //3<span class="num_delim"> </span>990 руб.
+                //</div> <div class="market_item_controls unshown">
+                foreach (var prod in listProducts)
+                {
+                    prod.Price = Regex.Replace(prod.Price, @"<span class=""num_delim"">\s*?</span>",
+                        " ");
+                    string desc = web.DownloadString(prod.Url);
+                    desc = System.Web.HttpUtility.HtmlDecode(desc);
+                    prod.Desc = Regex.Replace(desc, @".*?<div class=""market_item_description"">[\n]*?\s+(.*?)</div>(.*?)<div class=""market_item_controls unshown"">.*",
+                        "$1", RegexOptions.Singleline);
+                    prod.Desc = Regex.Replace(prod.Desc, @"<.*?>", " ", RegexOptions.Singleline);
+                    prod.Desc = Regex.Replace(prod.Desc, @"Показать полностью\.\.", "", RegexOptions.Singleline);
+                    prod.Desc = Regex.Replace(prod.Desc, @"\s+", " ", RegexOptions.Singleline);
+                    Thread.Sleep(Zaderzhka);
+                }
+                return listProducts;
             }
-            //<div id="market_item24046" class="market_row">
-            //File.WriteAllText("C:\\Users\\Maxs\\Desktop\\www1.txt", page);
-            //div[@class='market_row_inner_cont']
-            List<Product> listProducts = new List<Product>();
-
-            var tempUrlToItems = Regex.Matches(page, @"name""><a href=""(/market-[0-9]{2,20}\?w=product-[0-9_]{2,35})")
-            .Cast<Match>()
-            .Select(m => m.Groups[1].Value)
-            .ToArray();
-
-            var tempIdToItems = Regex.Matches(page, @"<div id=""market_item([0-9]{1,9})"" class=""market_row"">")
-            .Cast<Match>()
-            .Select(m => m.Groups[1].Value)
-            .ToArray();
-
-            var tempNameToItems = Regex.Matches(page, @"<div class=""market_row_name""><a.*?>(.*?)</a")
-            .Cast<Match>()
-            .Select(m => m.Groups[1].Value)
-            .ToArray();
-
-            var tempPriceToItems = Regex.Matches(page, @"<div class=""market_row_price"">(.*?)</div>")
-            .Cast<Match>()
-            .Select(m => m.Groups[1].Value)
-            .ToArray();
-
-            Product product;
-            for (int i = 0; i < tempUrlToItems.Length; i++)
-            {
-                product = new Product();
-                product.Url = Regex.Replace(tempUrlToItems[i], @"/(market-[0-9]{2,20})\?w=(product-[0-9_]{2,35})",
-                    "http://vk.com/wkview.php?act=show&al=1&loc=$1&w=$2");
-                product.Name = tempNameToItems[i];
-                product.Id = tempIdToItems[i];
-                //<div class="market_row_price">2<span class="num_delim"> </span>230 руб.</div>
-                product.Price = Regex.Replace(tempPriceToItems[i], @"(<div class=""market_row_price"".*?)([7-9]{1,7} руб.)(</div>)",
-                    "$2");
-
-                listProducts.Add(product);
-            }
-
-            listProducts = listProducts.Distinct(new ProductComparer()).ToList();
-
-
-            str1 = System.Web.HttpUtility.HtmlDecode(str1);
-
-            //3<span class="num_delim"> </span>990 руб.
-            //</div> <div class="market_item_controls unshown">
-            foreach (var prod in listProducts)
-            {
-                prod.Price = Regex.Replace(prod.Price, @"<span class=""num_delim"">\s*?</span>",
-                    " ");
-                string desc = web.DownloadString(prod.Url);
-                desc = System.Web.HttpUtility.HtmlDecode(desc);
-                prod.Desc = Regex.Replace(desc, @".*?<div class=""market_item_description"">[\n]*?\s+(.*?)</div>(.*?)<div class=""market_item_controls unshown"">.*",
-                    "$1", RegexOptions.Singleline);
-                prod.Desc = Regex.Replace(prod.Desc, @"<.*?>", " ", RegexOptions.Singleline);
-                prod.Desc = Regex.Replace(prod.Desc, @"Показать полностью\.\.", "", RegexOptions.Singleline);
-                prod.Desc = Regex.Replace(prod.Desc, @"\s+", " ", RegexOptions.Singleline);
-                Thread.Sleep(Zaderzhka);
-            }
-            return listProducts;
+            
         }
     }
 }
